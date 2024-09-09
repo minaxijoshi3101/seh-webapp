8b42bbf9a8907b325954a0771ef22428154648ea
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2024-09-09 20:49:51 +0800

    latest build pushed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,12 @@ import classImg from "../assets/images/slider/ClassRoom.jpg";
 import sehImg from "../assets/images/slider/seh.jpg";
 import jiraWFImg from "../assets/images/slider/JiraWorkflow.png"
 import ckaCertImg from "../assets/images/slider/CkaCert.jpg"
+import utetImg from "../assets/images/slider/UTET.jpg"
 
 const Home = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const imageArray = [classImg, sehImg, jiraWFImg, ckaCertImg];
+  const imageArray = [classImg, sehImg, jiraWFImg, ckaCertImg, utetImg];
 
   const handleNextImage = useCallback(() => {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageArray.length);
