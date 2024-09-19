6d11d8352d1070ca82aad1b21c84beda2ca301ef
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2024-09-19 23:12:22 +0800

    chatbot added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect, useCallback } from "react";
 import "../styles/AboutUs.css";
 import "../styles/Home.css";
 import ButtonComp from "../components/ButtonComp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
 import classImg from "../assets/images/slider/ClassRoom.jpg";
 import sehImg from "../assets/images/slider/seh.jpg";
-import jiraWFImg from "../assets/images/slider/JiraWorkflow.png"
-import ckaCertImg from "../assets/images/slider/CkaCert.jpg"
-import utetImg from "../assets/images/slider/UTET.jpg"
+import jiraWFImg from "../assets/images/slider/JiraWorkflow.png";
+import ckaCertImg from "../assets/images/slider/CkaCert.jpg";
+import utetImg from "../assets/images/slider/UTET.jpg";
 
 const Home = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isChatOpen, setIsChatOpen] = useState(false);
 
   const imageArray = [classImg, sehImg, jiraWFImg, ckaCertImg, utetImg];
 
@@ -27,6 +30,10 @@ const Home = () => {
     const interval = setInterval(handleNextImage, 3000);
     return () => clearInterval(interval);
   }, [handleNextImage]);
+
+  const toggleChat = () => {
+    setIsChatOpen((prev) => !prev);
+  };
 
   return (
     <div className="row">
@@ -46,6 +53,28 @@ const Home = () => {
           <ButtonComp type="submit" label="Enroll Now" />
         </div>
       </div>
+
+      {/* Chatbot icon in the bottom-right */}
+      <div className="chatbot-icon" onClick={toggleChat}>
+        <FontAwesomeIcon icon={faRobot} size="2x" />
+      </div>
+
+      {/* Chatbot UI */}
+      {isChatOpen && (
+        <div className="chatbot-box">
+          <div className="chatbot-header">
+            <h3>SEH Chatbot</h3>
+            <button onClick={toggleChat}>X</button>
+          </div>
+          <div className="chatbot-messages">
+            <p>Hi! How can I help you?</p>
+          </div>
+          <div className="chatbot-input">
+            <input type="text" placeholder="Type your message..." />
+            <button>Send</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
