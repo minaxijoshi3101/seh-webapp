889e798a362962a2e181e3083d6cdf4487650c2f
unknown <minaxijoshi3101@gmail.com>
2024-09-30 13:09:42 +0800

    image added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,12 +10,13 @@ import jiraWFImg from "../assets/images/slider/JiraWorkflow.png";
 import ckaCertImg from "../assets/images/slider/CkaCert.jpg";
 import utetImg from "../assets/images/slider/UTET.jpg";
 import sehImg1 from "../assets/images/slider/img1.jpg";
+import sehImg2 from "../assets/images/slider/sehImg2.avif";
 
 const Home = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [isChatOpen, setIsChatOpen] = useState(false);
 
-  const imageArray = [sehImg1, classImg, sehImg2, jiraWFImg, ckaCertImg, utetImg];
+  const imageArray = [sehImg1, sehImg2, sehImg2, jiraWFImg, ckaCertImg];
 
   const handleNextImage = useCallback(() => {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageArray.length);
